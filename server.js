51cbebb609395a09b4f11515f842a68609bc9e5f
LuisSalas59 <85508207+LuisSalas59@users.noreply.github.com>
2024-05-29 12:12:22 -0600

    Update server.js

--- server.js
+++ server.js
@@ -4,19 +4,19 @@ const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
-const IMAGES_DIR = path.join(__dirname, 'images');
+const IMAGES_DIR = path.join(__dirname, 'boletines');
 
 // Servir archivos estáticos desde el directorio de imágenes
-app.use('/images', express.static(IMAGES_DIR));
+app.use('/boletines', express.static(IMAGES_DIR));
 
 // Endpoint para listar todas las imágenes en el directorio
-app.get('/api/images', (req, res) => {
+app.get('/boletines', (req, res) => {
     fs.readdir(IMAGES_DIR, (err, files) => {
         if (err) {
             return res.status(500).send('Unable to scan directory');
         }
         const images = files.filter(file => /\.(jpg|jpeg|png|gif)$/i.test(file));
-        res.json(images);
+        res.json(boletines);
     });
 });
 
